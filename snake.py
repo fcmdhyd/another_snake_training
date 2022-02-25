4dd3ec21e729742343b2dc12ceadf6ad32f763bd
fcmdhyd <fcmdhyd@gmail.com>
2022-02-25 23:31:24 +0800

    Initial commit

---
+++ snake.py
@@ -0,0 +1,48 @@
+from turtle import Turtle
+
+STARTING_POSITION = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+class Snake(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.segment = []
+        self.create_snake()
+        self.head = self.segment[0]
+
+    def create_snake(self):
+        for position in STARTING_POSITION:
+            new_seg = Turtle('square')
+            new_seg.color('white')
+            new_seg.penup()
+            new_seg.goto(position)
+            self.segment.append(new_seg)
+
+    def move(self):
+        for seg_num in range(len(self.segment) - 1, 0, -1):
+            new_x = self.segment[seg_num - 1].xcor()
+            new_y = self.segment[seg_num - 1].ycor()
+            self.segment[seg_num].goto(new_x, new_y)
+
+        self.head.fd(MOVE_DISTANCE)
+
+    def go_up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def go_down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def go_left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def go_right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
