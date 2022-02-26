de75e1023f9c61693b65707e3ae5ad8f3c87fa26
fcmdhyd <fcmdhyd@gmail.com>
2022-02-27 01:01:46 +0800

    Update with scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -1,1 +1,24 @@
 from turtle import Turtle
+ALIGNMENT = 'left'
+FONT = ('Comic Sans',18,'normal')
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.goto(-280, 265)
+        self.penup()
+        self.hideturtle()
+        self.color("white")
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.write(f"Score: {self.score}", align = ALIGNMENT, font = FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
+
